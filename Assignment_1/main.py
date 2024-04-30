51fb9db86cc601730e1437ec3f41ac6683b719ed
Laura Maria Höber <laura.hoeber@student.tugraz.at>
2024-04-30 16:06:59 +0200

    final version

--- Assignment_1/main.py
+++ Assignment_1/main.py
@@ -13,120 +13,40 @@ from matplotlib.backends.backend_pdf import PdfPages
 
 from scipy.integrate import quad
 
-########################################## Helper functions ##########################################
-def tol_inverse(A):
-    #add small offset to main diagonal
-    return np.linalg.inv(A+np.eye(A.shape[1])*(1e-6))
 
-def log_cholesky_det_sqrt(A):
-    lower = np.linalg.cholesky(A+np.eye(A.shape[1])*(1e-6))
-    #squareroot of the determinant of A
-    sign, logdet = np.linalg.slogdet(lower)
-    return sign*logdet
-
-def single_gauss(x_s, mu, sigma):
-    #take the pseudo-inverse only in case the matrix is not invertible
-    exponent = -1/2*((x_s-mu).T @ tol_inverse(sigma) @ (x_s-mu))
-    #The factor in front of determinant (2*np.pi)**(x_s.shape[0]/2) leads to overflow error
-    #for the responsabilities it cancels out anyway
-    factor = 1/(log_cholesky_det_sqrt(sigma))
-    return factor * np.exp(exponent)
-
-def cost_function(x, mu_2,sigma_2,pi_2, mu_1,sigma_1,pi_1):
-    D = sigma_2.shape[1]
-    sum_terms_1 = 0
-    sum_terms_2 = 0
-
-    inv_sig_1 = [tol_inverse(sigma_1[k]) for k in range(K)]
-    log_det_sig_sqrt_1 = [log_cholesky_det_sqrt(sigma_1[k]) for k in range(K)]
-
-    inv_sig_2 = [tol_inverse(sigma_2[k]) for k in range(K)]
-    log_det_sig_sqrt_2 = [log_cholesky_det_sqrt(sigma_2[k]) for k in range(K)]
-    for s in range(x.shape[0]):
-        
-        exponents1 = [np.log(pi_1[k])-D/2*np.log(2*np.pi)-log_det_sig_sqrt_1[k]-1/2*((x[s,:]-mu_1[k]).T @ inv_sig_1[k] @ (x[s,:]-mu_1[k])) for k in range(K)]
-        exponents2 = [np.log(pi_2[k])-D/2*np.log(2*np.pi)-log_det_sig_sqrt_2[k]-1/2*((x[s,:]-mu_2[k]).T @ inv_sig_2[k] @ (x[s,:]-mu_2[k])) for k in range(K)]
-        
-        sum_terms_1 += np.max(exponents1) + np.log(np.sum([np.exp(exponents1[k]-np.max(exponents1)) for k in range(K)]))
-        sum_terms_2 += np.max(exponents2) + np.log(np.sum([np.exp(exponents2[k]-np.max(exponents2)) for k in range(K)]))
-        
-    cost = np.abs(sum_terms_1-sum_terms_2)
-    
-    return cost
-
-def stable_cond_weights(x,weights,mu, sigma):
-    posts = []
-    for i in range(len(weights)):
-
-        dim = sigma[0].shape[0]
-        exponents = [np.log(weights[k])-dim/2*np.log(2*np.pi)-log_cholesky_det_sqrt(sigma[k])-1/2*((x-mu[k]).T @ tol_inverse(sigma[k]) @ (x-mu[k])) for k in range(K)]
-                    
-        y_max = np.max(exponents)
-        # -> log sum trick: adding the first maximum in front
-        log_post_enum = exponents[i]
-        log_post_denum = y_max + np.log(np.sum([np.exp(exponents[k]-y_max) for k in range(K)]))
-        posts.append(log_post_enum-log_post_denum)
-        
-    return  [np.exp(post) for post in posts]
-
-
-def plot_GMM(mu, sigma, pi,ax):
-    K = len(mu)
-
-    # Plot each GMM component
-    for k, (mean, cov, weight) in enumerate(zip(mu, sigma, pi)):
-        # Plot the mean
-        M = int(np.sqrt(len(mean)))
-        mean_reshaped = mean.reshape(M,M)
-        ax[0,k].imshow(mean_reshaped, cmap='gray', interpolation='nearest')
-        ax[0,k].set_title(fr'k = {k}, $\pi_{k}$ = {pi[k]}')
-
-        # Plot the covariance matrix
-        ax[1,k].imshow(cov, cmap='viridis', interpolation='nearest')
-
-    return ax 
-
-def plot_samples(mu, sigma, pi, ax, num):
-    
-    
-    #TODO: drawing image, that is assigned to this component?
-    for i in range(num//2):
-        for j in range(2):
-            #choosing random component
-            prop = np.random.uniform()
-            threshes = np.cumsum(pi)
-            k= 0
-            for u,t in enumerate(threshes):
-                if prop<=t:
-                    k=u
-                    break
-
-            #########
-            # transforming random sample from a standard normal distribution into a sample from our distribution
-            # this is essentially the reverse calculation to the z-transform z = (x-mu)/sigma  ->  x = mu +sigma*z
-            # the cholesky decomposition of the determinant is like taking its root
-            sample_norm = np.random.randn(mu.shape[1])
-            lower = np.linalg.cholesky(sigma[k,:,:]+ np.eye(sigma.shape[1])*(1e-6))
-            sample = mu[k,:] + lower @ sample_norm
-            #########
-            M = int(np.sqrt(len(sample)))
-            sample_reshaped = sample.reshape(M,M)
-            im = ax[j,i].imshow(sample_reshaped, cmap='gray', interpolation='nearest')
-            # Add a colorbar for covariance matrix
-            #plt.gcf().colorbar(im, ax=ax[0,k])
-            ax[j,i].set_title(f'k = {k}')
-
-
-    return ax
-########################################## Helper functions ##########################################
 
 def task1():
     # probability density functions with change of variables, check that you obtain a valid transformed pdf
     
     """ Start of your code
     """
+    # probability density functions with change of variables, check that you obtain a valid transformed pdf
     
-    
+    """ Start of your code
+    """
+    py = lambda y: np.exp(-0.5*y)/(np.sqrt(2*np.pi)*np.sqrt(y))
+
+    # Plot the transformed pdf
+    x = np.linspace(0.01, 5, 100)  # Avoid zero for plotting
+    plt.plot(x, py(x))
+    plt.xlabel('y')
+    plt.ylabel('p$_Y$(y)')
+    plt.title('Transformed PDF p$_Y$(y)')
+    plt.grid()
+    plt.xlim([-2, 5])
+    #plt.show()
+    plt.savefig('transformed_pdf_plot.png')
+
+    # Integrate the pdf over different ranges
+    inf_inf = quad(py, -np.inf, np.inf)
+    zero_inf = quad(py, 0, np.inf)
+    inf_zero = quad(py, -np.inf, 0)
+
+    # Print the integration results
+    print('from -inf to inf:', inf_inf)
+    print('from 0 to inf:', zero_inf)
+    print('from -inf to 0:', inf_zero)
+        
     """ End of your code
     """
 
@@ -152,6 +72,88 @@ def task2(x, K):
 
     """ Start of your code
     """
+    ########################################## Start Helper functions ##########################################
+    def tol_inverse(A):
+        #add small offset to main diagonal
+        return np.linalg.inv(A+np.eye(A.shape[1])*(1e-6))
+
+    def log_cholesky_det_sqrt(A):
+        lower = np.linalg.cholesky(A+np.eye(A.shape[1])*(1e-6))
+        #squareroot of the determinant of A
+        sign, logdet = np.linalg.slogdet(lower)
+        return sign*logdet
+
+    def cost_function(x, mu_2,sigma_2,pi_2, mu_1,sigma_1,pi_1):
+        D = sigma_2.shape[1]
+        sum_terms_1 = 0
+        sum_terms_2 = 0
+
+        inv_sig_1 = [tol_inverse(sigma_1[k]) for k in range(K)]
+        log_det_sig_sqrt_1 = [log_cholesky_det_sqrt(sigma_1[k]) for k in range(K)]
+
+        inv_sig_2 = [tol_inverse(sigma_2[k]) for k in range(K)]
+        log_det_sig_sqrt_2 = [log_cholesky_det_sqrt(sigma_2[k]) for k in range(K)]
+        for s in range(x.shape[0]):
+            
+            exponents1 = [np.log(pi_1[k])-D/2*np.log(2*np.pi)-log_det_sig_sqrt_1[k]-1/2*((x[s,:]-mu_1[k]).T @ inv_sig_1[k] @ (x[s,:]-mu_1[k])) for k in range(K)]
+            exponents2 = [np.log(pi_2[k])-D/2*np.log(2*np.pi)-log_det_sig_sqrt_2[k]-1/2*((x[s,:]-mu_2[k]).T @ inv_sig_2[k] @ (x[s,:]-mu_2[k])) for k in range(K)]
+            
+            sum_terms_1 += np.max(exponents1) + np.log(np.sum([np.exp(exponents1[k]-np.max(exponents1)) for k in range(K)]))
+            sum_terms_2 += np.max(exponents2) + np.log(np.sum([np.exp(exponents2[k]-np.max(exponents2)) for k in range(K)]))
+            
+        cost = np.abs(sum_terms_1-sum_terms_2)
+        
+        return cost
+
+    def plot_GMM(mu, sigma, pi,ax):
+        K = len(mu)
+
+        # Plot each GMM component
+        for k, (mean, cov, weight) in enumerate(zip(mu, sigma, pi)):
+            # Plot the mean
+            M = int(np.sqrt(len(mean)))
+            mean_reshaped = mean.reshape(M,M)
+            ax[0,k].imshow(mean_reshaped, cmap='gray', interpolation='nearest')
+            ax[0,k].set_title(fr'k = {k}, $\pi_{k}$ = {pi[k]}')
+
+            # Plot the covariance matrix
+            ax[1,k].imshow(cov, cmap='viridis', interpolation='nearest')
+
+        return ax 
+
+    def plot_samples(mu, sigma, pi, ax, num):
+
+        for i in range(num//2):
+            for j in range(2):
+                #choosing random component
+                prop = np.random.uniform()
+                threshes = np.cumsum(pi)
+                k= 0
+                for u,t in enumerate(threshes):
+                    if prop<=t:
+                        k=u
+                        break
+
+                #########
+                # transforming random sample from a standard normal distribution into a sample from our distribution
+                # this is essentially the reverse calculation to the z-transform z = (x-mu)/sigma  ->  x = mu +sigma*z
+                # the cholesky decomposition of the determinant is like taking its root
+                sample_norm = np.random.randn(mu.shape[1])
+                lower = np.linalg.cholesky(sigma[k,:,:]+ np.eye(sigma.shape[1])*(1e-6))
+                sample = mu[k,:] + lower @ sample_norm
+                #########
+                M = int(np.sqrt(len(sample)))
+                sample_reshaped = sample.reshape(M,M)
+                im = ax[j,i].imshow(sample_reshaped, cmap='gray', interpolation='nearest')
+                # Add a colorbar for covariance matrix
+                #plt.gcf().colorbar(im, ax=ax[0,k])
+                ax[j,i].set_title(f'k = {k}')
+
+
+        return ax
+    ########################################## End Helper functions ##########################################
+
+
     S = x.shape[0] #2000... number of training images
     M = x.shape[1] #28... width of one image
     D = M*M        #784... number of pixels in an image
@@ -332,6 +334,38 @@ def task3(x, mask, m_params):
 
     """ Start of your code
     """
+    
+    ########################################## Start Helper functions ##########################################
+    def tol_inverse(A):
+        #add small offset to main diagonal
+        return np.linalg.inv(A+np.eye(A.shape[1])*(1e-6))
+
+    def log_cholesky_det_sqrt(A):
+        lower = np.linalg.cholesky(A+np.eye(A.shape[1])*(1e-6))
+        #squareroot of the determinant of A
+        sign, logdet = np.linalg.slogdet(lower)
+        return sign*logdet
+
+
+    def stable_cond_weights(x,weights,mu, sigma):
+        posts = []
+        for i in range(len(weights)):
+
+            dim = sigma[0].shape[0]
+            exponents = [np.log(weights[k])-dim/2*np.log(2*np.pi)-log_cholesky_det_sqrt(sigma[k])-1/2*((x-mu[k]).T @ tol_inverse(sigma[k]) @ (x-mu[k])) for k in range(K)]
+                        
+            y_max = np.max(exponents)
+            # -> log sum trick: adding the first maximum in front
+            log_post_enum = exponents[i]
+            log_post_denum = y_max + np.log(np.sum([np.exp(exponents[k]-y_max) for k in range(K)]))
+            posts.append(log_post_enum-log_post_denum)
+            
+        return  [np.exp(post) for post in posts]
+
+
+    ########################################## End Helper functions ##########################################
+
+
 
     x_2_indices = np.where(mask)[0]
     x_1_indices = np.where([1-n for n in mask])[0]
