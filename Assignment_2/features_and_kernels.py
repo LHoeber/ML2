65728bd81cd8ac45a9cd708d3fdaa458cd97bbca
Laura Maria Höber <laura.hoeber@student.tugraz.at>
2024-05-19 12:33:32 +0200

    implemented task 2

--- Assignment_2/features_and_kernels.py
+++ Assignment_2/features_and_kernels.py
@@ -180,7 +180,94 @@ def task2():
 
     """ Start of your code 
     """
+    lam = 0.1
 
+    def z_kernel_fourier(X,R):
+        #transforming X with N samples into Z with R samples
+        W = np.random.multivariate_normal(np.zeros(D),np.eye(D),R)
+        B = [np.random.uniform(0,2*np.pi) for r in range(R)]
+        
+        Z = np.sqrt(2 / R) * np.cos(X @ W.T + B)
+        return Z
+
+    def z_kernel_gauß(X,R):
+        N = X.shape[0]
+        #transforming X with N samples into Z with R samples
+        #TODO: can the same samples be drawn multiple times?
+        T = X[np.random.choice(np.arange(0,X.shape[0]), R)]
+        sigma = 1
+        Z = np.zeros((N, R))
+        
+        for r in range(R):
+            t = T[r]
+            prod = -np.linalg.norm((X-t),axis=1)**2
+            Z[:, r] = np.sqrt(1 / R) * np.exp(prod/ (2 * sigma**2))
+        
+        return Z
+
+
+    def theta_star(x_train,y_train,lam,method,R):
+        if method == "fourier":
+            z = z_kernel_fourier(x_train,R)
+
+        elif method == "gauss":
+            z =  z_kernel_gauß(x_train,R)
+            
+        else:
+            raise NameError(f"'{method}' is not a valid method to calculate the kernel.")
+        term_1 = z @ z.T +lam*np.eye(z.shape[0])
+        inv_term_1 = np.linalg.pinv(term_1)
+        term_2 = inv_term_1.T @ z
+        return term_2.T @ np.reshape(y_train,(len(y_train),1))
+
+    def y_hat(x,theta,method):
+        R = theta.shape[0]
+        if method == "fourier":
+            z = z_kernel_fourier(x,R)
+
+        elif method == "gauss":
+            z =  z_kernel_gauß(x,R)
+        return z.T @ z @ theta
+    
+    def mse(y,y_pred):
+        return np.mean(np.power((y-y_pred),2))
+
+    R_list = [1,2,10,20,50,100]
+    runs = 5
+    #losses for each R and 5 runs
+    fourier_training_loss = np.zeros((runs,len(R_list)))
+    gauss_training_loss = np.zeros((runs,len(R_list)))
+
+    fourier_test_loss = np.zeros((runs,len(R_list)))
+    gauss_test_loss = np.zeros((runs,len(R_list)))
+
+    for iter in range(runs):
+        for r,R in enumerate(R_list):  
+            # With random fourier feature transform:
+            theta_fourier = theta_star(x,y,lam,"fourier",R)
+            y_pred = y_hat(x,theta_fourier,"fourier")
+            fourier_training_loss[iter,r] = mse(y,y_pred)
+
+            y_pred = y_hat(x_test,theta_fourier,"fourier")
+            fourier_test_loss[iter,r] = mse(y,y_pred)
+
+            # with random gauss feature transform:
+            theta_gauss = theta_star(x,y,lam,"gauss",R)
+            y_pred = y_hat(x,theta_gauss,"gauss")
+            gauss_training_loss[iter,r] = mse(y,y_pred)
+
+            y_pred = y_hat(x_test,theta_gauss,"gauss")
+            gauss_test_loss[iter,r] = mse(y,y_pred)
+
+    fourier_training_loss = np.mean(fourier_training_loss,axis = 0)
+    gauss_training_loss = np.mean(gauss_training_loss,axis = 0)
+    ax[0].plot(fourier_training_loss,label="training set")
+    ax[1].plot(gauss_training_loss,label="training set")
+
+    fourier_test_loss = np.mean(fourier_test_loss,axis = 0)
+    gauss_test_loss = np.mean(gauss_test_loss,axis = 0)
+    ax[0].plot(fourier_test_loss,label="test set")
+    ax[1].plot(gauss_test_loss,label="test set")
 
 
     """ End of your code 
